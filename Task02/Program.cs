695e6a60b66855d7f340733381fb1d842b03bf63
Artem Kalugin <aokalugin@edu.hse.ru>
2020-04-28 23:29:50 +0300

    All tasks done

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,34 +1,35 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
+/* 
 В основной программе объявите и инициализируйте одномерный строковый массив 
 и выполните циклический перебор его элементов с разных «начальных точек», 
-разделяя элементы одним пробелом.
+разделяя элементы одним пробелом. 
 
-Тестирование приложения выполняется путем запуска разных наборов тестов.
+Тестирование приложения выполняется путем запуска разных наборов тестов. 
 На вход в первой строке поступает число - номер элемента, начиная с которого 
-пойдет циклический перебор.
+пойдет циклический перебор. 
 В следующей строке указаны элементы последовательности, разделенные одним или 
-несколькими пробелами.
-3
-1 2 3 4 5
-Программа должна вывести на экран:
-3 4 5 1 2
+несколькими пробелами. 
+3 
+1 2 3 4 5 
+Программа должна вывести на экран: 
+3 4 5 1 2 
 
-В случае ввода некорректных данных выбрасывайте ArgumentException.
+В случае ввода некорректных данных выбрасывайте ArgumentException. 
 
-Никаких дополнительных символов выводиться не должно.
+Никаких дополнительных символов выводиться не должно. 
 
-Код метода Main можно подвергнуть изменениям, но вывод меняться не должен.
+Код метода Main можно подвергнуть изменениям, но вывод меняться не должен. 
 
- */
+*/
 namespace Task02
 {
-    class IteratorSample : IEnumerable<string> // НЕ МЕНЯТЬ
+    class IteratorSample : IEnumerable<string> // НЕ МЕНЯТЬ 
     {
         string[] values;
         int start;
@@ -38,7 +39,67 @@ namespace Task02
             this.values = values;
             this.start = start;
         }
+        public IEnumerator GetEnumerator()
+        {
+            return new IteratorSampleEnumerator(values, start - 2);
+        }
 
+        IEnumerator<string> IEnumerable<string>.GetEnumerator()
+        {
+            throw new ArgumentException();
+        }
+    }
+    class IteratorSampleEnumerator : IEnumerator
+    {
+
+        string[] values;
+        int position;
+        int startposition;
+        bool loop;
+
+        public IteratorSampleEnumerator(string[] values, int start)
+        {
+            this.values = values;
+            position = start;
+            startposition = start;
+            loop = false;
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (position == -1 || position >= values.Length)
+                    throw new ArgumentException();
+                return values[position];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (position != startposition || !loop)
+            {
+                if (position < values.Length - 1)
+                {
+                    position++;
+                    return true;
+                }
+                if (position == values.Length - 1)
+                {
+                    loop = true;
+                    position = 0;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
     }
 
     class Program
@@ -47,11 +108,16 @@ namespace Task02
         {
             try
             {
-                int startingIndex = 
-                string[] values = 
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.InputEncoding = Encoding.UTF8;
 
+                int startingIndex = ParseToInt();
+                string[] values = Console.ReadLine().Split();
+                if (string.IsNullOrWhiteSpace(values[0]) || values.Length < startingIndex)
+                    throw new ArgumentException();
                 foreach (string ob in new IteratorSample(values, startingIndex))
                     Console.Write(ob + " ");
+                Console.WriteLine();
             }
             catch (ArgumentException e)
             {
@@ -59,10 +125,20 @@ namespace Task02
             }
             catch (Exception e)
             {
-                Console.WriteLine("problem");
+                Console.WriteLine("error");
             }
 
             Console.ReadLine();
+        }
+
+        //Преобразование строки в чесло с необходимой проверкой
+        public static int ParseToInt()
+        {
+            string str = Console.ReadLine();
+            int num;
+            if (!int.TryParse(str, out num))
+                throw new ArgumentException();
+            return num;
         }
     }
 }
--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-
 /* На вход подается число N.
  * Нужно создать коллекцию из N квадратов последовательного ряда натуральных чисел 
  * и вывести ее на экран дважды. Элементы коллекции разделять пробелом. 
@@ -19,6 +18,7 @@ using System.Collections;
  * 
  * В случае ввода некорректных данных выбрасывайте ArgumentException.
 */
+
 namespace Task04
 {
     class Program
@@ -27,35 +27,66 @@ namespace Task04
         {
             try
             {
-                int value = 
-                MyInts myInts = new MyInts();
-                IEnumerator enumerator = myInts.MyEnumerator(value);
-
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                int num = ParseToInt();
+                MyInts myInts = new MyInts(num);
+                IEnumerator enumerator = myInts.MyEnumerator(num);
+                Iterator(enumerator, num);
                 Console.WriteLine();
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                Iterator(enumerator, num);
             }
             catch (ArgumentException)
             {
                 Console.WriteLine("error");
             }
-            
         }
 
-        static void IterateThroughEnumeratorWithoutUsingForeach(IEnumerator enumerator)
+        //вывод коллекции на экран
+        static void Iterator(IEnumerator enumerator, int n)
         {
+            for (int i = 0; i < n; i++)
+            {
+                enumerator.MoveNext();
+                Console.Write($"{enumerator.Current}");
+                if (i != n - 1) Console.Write(" ");
+            }
+        }
+
+        //Перевод строки в число с соответствующей проверкой
+        public static int ParseToInt()
+        {
+            string strs = Console.ReadLine();
+            int res;
+            if (!int.TryParse(strs, out res) || res < 0) throw new ArgumentException();
+            return res;
         }
     }
 
     class MyInts : IEnumerator // НЕ МЕНЯТЬ ЭТУ СТРОКУ
     {
-        
+        int N, position;
+        public MyInts(int n) { N = n; position = 0; }
         public bool MoveNext()
         {
+            if (position < N)
+            {
+                position++;
+                return true;
+            }
+            Reset();
+            return false;
         }
+
+        public void Reset() { position = 1; }
+
+        internal IEnumerator MyEnumerator(int value) { return new MyInts(value); }
 
         public object Current
         {
+            get
+            {
+                if (position == -1 || position > N) throw new InvalidOperationException();
+                return Math.Pow(position, 2);
+            }
         }
     }
 }
